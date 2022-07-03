87cf2b9d253a8be62921ef4cf71682f035b2e230
Jisu Han <weblite@naver.com>
2022-07-03 23:54:28 +0900

    로그인 처리 1 쿠키 세션 - 로그인 기능

---
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,27 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    // 로그인 제어
+    public Member login(String loginId, String password) {
+        Optional<Member> loginMember = memberRepository.findByLoginId(loginId);
+        Member member = loginMember.get();
+        if (member.getPassword().equals(password)) {
+            return member;
+        } else {
+            return null;
+        }
+
+    }
+}
--- login/src/main/java/hello/login/web/member/MemberController.java
+++ login/src/main/java/hello/login/web/member/MemberController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.validation.Valid;
 
 @Controller
@@ -36,5 +35,6 @@ public class MemberController {
         memberRepository.save(member);
         return "redirect:/";
     }
+
 
 }
