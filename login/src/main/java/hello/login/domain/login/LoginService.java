067c98f56c9882cf5f9817cc142bdb301b7112de
Jisu Han <weblite@naver.com>
2022-07-03 23:57:19 +0900

    로그인 처리 1 쿠키 세션 - 로그인 기능 - Optional Stream

--- login/src/main/java/hello/login/domain/login/LoginService.java
+++ login/src/main/java/hello/login/domain/login/LoginService.java
@@ -15,6 +15,9 @@ public class LoginService {
 
     // 로그인 제어
     public Member login(String loginId, String password) {
+        /*
+
+
         Optional<Member> loginMember = memberRepository.findByLoginId(loginId);
         Member member = loginMember.get();
         if (member.getPassword().equals(password)) {
@@ -22,6 +25,15 @@ public class LoginService {
         } else {
             return null;
         }
+
+
+         */
+
+        // optional 스트림 으로 바꾸기 Optional
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+
 
     }
 }
