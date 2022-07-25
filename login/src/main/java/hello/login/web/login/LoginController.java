1a5f642d1c56f114e339a67638db269359e1a19b
First-jisu-Han <weblite@naver.com>
2022-07-25 17:58:45 +0900

    쿠키 생성

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -22,7 +24,7 @@ public class LoginController {
         return "login/LoginForm";
     }
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form , BindingResult bindingResult){
+    public String login(@Valid @ModelAttribute LoginForm form , BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
         }
@@ -31,8 +33,9 @@ public class LoginController {
             bindingResult.reject("loginFail","아이디또는 비밀번호가 틀립니다.");
             return "login/loginForm";
         }
-        // 로그인 성공
-
+        // 로그인 성공 , 쿠키 부여
+        Cookie IdCookie=new Cookie("memberId",String.valueOf(loginMember.getLoginId()));
+        response.addCookie(IdCookie); // 서블릿 응답에 쿠키를 담기 - 세션쿠키: 웹브라우저 종료시 소멸, 웹브라우저 지속해서 Cookie를 보내줌
         return "redirect:/";
     }
 }
