12bea5d13cf73253977d5deae97b4ba025bc5806
First-jisu-Han <weblite@naver.com>
2023-01-13 23:59:32 +0900

    직접만든 세션 적용 - HomeController , LoginController 수정2

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -18,39 +18,21 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
 
     @Autowired
-    public LoginService loginService;
+    LoginService loginService;
     @Autowired
-    public MemberRepository memberRepository;
+    MemberRepository memberRepository;
 
     @Autowired
-    public SessionManager sessionMananger;
+    SessionManager sessionMananger;
 
 
 
-    // 쿠키가 없어도 홈화면 접근 되도록 설정 - required=false
-//    @GetMapping("/")
-//    public String homeLogin(@CookieValue(name="memberId", required = false )Long memberId, Model model){
-//
-//        if(memberId==null) return "home";
-//
-//        Member loginMember=memberRepository.findById(memberId);
-//        loginMember.getLoginId();
-//        if(loginMember==null){ //쿠키가 소멸되어있는 경우가 있을 수 있음
-//              return "home";
-//        }
-//
-//        // 로그인 성공 로직
-//        model.addAttribute ("member",loginMember);
-//        return "loginHome";
-
+     쿠키가 없어도 홈화면 접근 되도록 설정 - required=false
     @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model){
+    public String homeLogin(@CookieValue(name="memberId", required = false )Long memberId, Model model){
 
-        // session 관리자에 저장된 회원정보 조회
-        Object session=sessionMananger.getSession(request);
+        if(memberId==null) return "home";
 
-
-        // 로그인
         Member loginMember=memberRepository.findById(memberId);
         loginMember.getLoginId();
         if(loginMember==null){ //쿠키가 소멸되어있는 경우가 있을 수 있음
@@ -60,6 +42,27 @@ public class HomeController {
         // 로그인 성공 로직
         model.addAttribute ("member",loginMember);
         return "loginHome";
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model){
+
+        // session 관리자에 저장된 회원정보 조회
+        Object session=sessionMananger.getSession(request);
+
+
+        // 로그인
+        Member loginMember=memberRepository.findById(memberId);
+        loginMember.getLoginId();
+        if(loginMember==null){ //쿠키가 소멸되어있는 경우가 있을 수 있음
+            return "home";
+        }
+
+        // 로그인 성공 로직
+        model.addAttribute ("member",loginMember);
+        return "loginHome";
+
+
+
 
 
 
